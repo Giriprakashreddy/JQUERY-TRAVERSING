0b643e2b4011a2991953c8b09ee83d1b1c2f9dae
giriprakashreddy <giriprakash523@gmail.com>
2018-08-13 13:29:11 -0500

    first commit

---
+++ js/all.js
@@ -0,0 +1,45 @@
+// $(document).ready(function () {
+//     $("#test").click(function() {
+//         $("#para").parentsUntil("#para","#demo-child").css({border:"1px solid red"});
+
+//     });
+ 
+// });
+// console.log(para.parent());
+
+// $(document).ready(function () {
+//     $("#test").click(function() {
+//         $("#para").prepend("Hello");
+
+//     });
+ 
+// });
+
+// $(document).ready(function () {
+//     $("#test").click(function() {
+//         $("#demo").empty();
+
+//     });
+ 
+// });
+
+// $(document).ready(function () {
+//     $("#test").click(function() {
+//         $("#demo").addClass("prop");
+//         $("p").addClass("prop1");
+
+//     });
+//     $("#test1").click(function() {
+//         $("#demo").removeClass("prop");
+
+//     });
+ 
+// });
+
+$(document).ready(function () {
+        $("#test").click(function() {
+            $("#demo").width();
+            $("#demo").innerWidth();
+    
+        });
+    });
